f97a3a5e1b1c506183684bbc47ed98db18ec878b
shadpre <klavs@tranborg.eu>
2023-03-06 10:49:59 +0100

    INIT

---
+++ src/dk/easv/bll/bot/mrgl_kte.java
@@ -0,0 +1,17 @@
+package dk.easv.bll.bot;
+
+import dk.easv.bll.game.IGameState;
+import dk.easv.bll.move.IMove;
+
+public class mrgl_kte extends mrglmrgl{
+
+    @Override
+    public IMove doMove(IGameState state) {
+        return null;
+    }
+
+    @Override
+    public String getBotName() {
+        return "Mrgl-KTE";
+    }
+}
